892f55846a792fb006e38c9b9a31d40f0537edd2
p.rosumaka <p.rosumaka@kernel.ua>
2021-12-09 21:36:06 +0200

    add p, s

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def fn2(**c):
     for x in c.values():
         z = z * x
     return z
-default_args = {'c': 5, 'd': 2, 'e': 20}
+default_args = {'c': 5, 'd': 2, 'e': 20, 'p': 50, 's': 100}
 print(fn2(**default_args))
 
 print("it's OK")
